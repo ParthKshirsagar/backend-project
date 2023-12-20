ca8bf2f1964baadf383a7c7ef60646776b2efe05
Parth Kshirsagar <parthkshirsagar96@gmail.com>
2023-12-20 21:58:03 +0530

    All setup configured

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs'
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret:  process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (filePath)=>{
+    try {
+        if(!filePath) return null
+        
+        //uploading file on cloudinary
+        const response = await cloudinary.uploader.upload(filePath, {
+            resource_type: 'auto'
+        })
+        //file uploaded successfully 
+        console.log("File is uploaded on cloudinary successfully! ", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(filePath); //Removes the file temporarily saved on server as the upload operation is failed
+    }
+}
+
+export {uploadOnCloudinary};
